564c65e5c1feb385ff2c8dcf9625e31e88a62349
Anant Kumar <anantk29082001@gmail.com>
2020-01-10 01:03:50 +0530

    Completed Rusted Email

--- task-7/Rusted_Email.rs
+++ task-7/Rusted_Email.rs
@@ -5,8 +5,8 @@ use std::io;
 fn main() 
 {
     let mut input = String::new();
-    println!("Please enter the email to check :")
-    match io::stdin().read_line(&mut input)
+    println!("Please enter the email to check :");
+    let mut user:String = io::stdin().read_line(&mut input).unwrap().to_string();
     let check= Regex::new(r"^([a-z0-9_+]([a-z0-9_+.]*[a-z0-9_+])?)@([a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6})").unwrap();
     
  
