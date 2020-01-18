4f41f0f364ddd95184aa86788ccc71080545d012
Rathinn <rathinnnaiir77@gmail.com>
2020-01-19 00:44:38 +0530

    Completed 2.py

--- task-13/2.py
+++ task-13/2.py
@@ -1,24 +1,32 @@
-def fibonnaci():
-    times=[]
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def create_fib(d):
+  temp=[]
+  a=1
+  b=2
+  c=0
+  while c<=d:
+    c=a+b
+    a=b
+    b=c
+    temp.append(a)
+  return(temp)
 
 n=int(input())
-arr=[]
-for i in range (n):
-    x=int(input())
-    arr.append(x)
+ipt=[]
+for i in range(n):
+  a=int(input())
+  ipt.append(a)
+
+
+fibonacci=[]
+for j in range(len(ipt)):
+  s=create_fib(ipt[j])
+  fibonacci.append(s)
+
+output=[]
+for k in range(len(fibonacci)):
+  sum=0
+  for l in range(len(fibonacci[k])):
+    if(fibonacci[k][l]%2==0):
+      sum+=fibonacci[k][l]
+  print(sum)
 
